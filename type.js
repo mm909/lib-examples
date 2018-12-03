52be1d94b2b8c721040e1bffd2e6aaa4d2f9495c
Mikian Musser <Bobar312@gmail.com>
2018-12-03 13:57:07 -0800

    Updated colors added countup

--- type.js
+++ type.js
@@ -2,13 +2,14 @@ jQuery(document).ready(function() {
   var type1 = new Typed('#type1', {
     strings: [
       ' <h1>This is an example of typed.js</h1>',
-      ' <h1>The green dots are an example of particles.js</h1>',
+      ' <h1>The dots/lines are an example of particles.js</h1>',
+      ' <h1>Below is an example of countup.js</h1>',
       ' <h1>Want to learn how to add these to your website?</h1>',
       ' <h1>Ask an instructor!</h1>',
     ],
     typeSpeed: 10,
     backSpeed: 10,
-    backDelay: 5000,
+    backDelay: 3000,
     loop: true,
   });
 });
